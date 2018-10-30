5917bfb1d60648813a8010939947d7b655751137
TTonggithub <44544505+TTonggithub@users.noreply.github.com>
2018-10-31 03:33:59 +1100

    Comment Functions (Foundation)
    Add:
    Model: Comment.java
    Interface: CommentManager.java
    A implementation of CommentManager: SimpleCommentManager.java

---
+++ springapp/src/main/java/au/usyd/elec5619/service/CommentManager.java
@@ -0,0 +1,20 @@
+package au.usyd.elec5619.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import au.usyd.elec5619.domain.Comment;
+
+public interface CommentManager extends Serializable {
+	
+	public List<Comment> getAllComments();
+	 	  
+	public void addComment(Comment comment);
+	public void deleteComment(long id) throws Exception;
+	public void updateComment(Comment comment);
+	
+	public Comment       getCommentById(long id);
+	public List<Comment> getCommentsByKeyWord(String keyWord);
+	public List<Comment> getCommentsByUser(long userId);
+	
+}
---
+++ springapp/src/main/java/au/usyd/elec5619/service/SimpleCommentManager.java
@@ -0,0 +1,76 @@
+package au.usyd.elec5619.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import au.usyd.elec5619.domain.Comment;
+
+@Service(value="simpleCommentManager")
+@Transactional
+public class SimpleCommentManager implements CommentManager {
+	
+	@Resource(name="sessionFactory")
+	private SessionFactory sessionFactory;
+	
+	@Autowired
+	public void setSessionFactory(SessionFactory sf) {
+		this.sessionFactory = sf;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Comment> getAllComments() {
+		// TODO Unsolved potential type safety issue
+		System.out.println("getAll sf:" + this.sessionFactory);
+		return this.sessionFactory.getCurrentSession().createQuery("FROM Comment").list();
+	}
+
+	@Override
+	public void addComment(Comment comment) {
+		System.out.println("add sf:" + this.sessionFactory);
+		this.sessionFactory.getCurrentSession().save(comment);
+	}
+
+	@Override
+	public void deleteComment(long id) throws Exception {
+		Comment comment = this.getCommentById(id);
+		if ( comment.getId() == id ) {
+			this.sessionFactory.getCurrentSession().delete(comment);
+		} else {
+			throw new Exception("Comment not found");
+		}
+	}
+	
+	@Override
+	public void updateComment(Comment comment) {
+		this.sessionFactory.getCurrentSession().update(comment);
+	}
+
+	@Override
+	public Comment getCommentById(long id) {
+		return (Comment) this.sessionFactory.getCurrentSession().get(Comment.class, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Comment> getCommentsByKeyWord(String keyWord) {
+		// TODO Unsolved potential type safety issue
+		String hql = String.format("FROM Comment where subject like %%s% or content like %%s%", keyWord, keyWord);
+		return this.sessionFactory.getCurrentSession().createQuery(hql).list();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Comment> getCommentsByUser(long userId) {
+		// TODO Unsolved potential type safety issue
+		String hql = String.format("FROM Comment where userId = %d", userId);
+		return this.sessionFactory.getCurrentSession().createQuery(hql).list();
+	}
+
+}
